a456f8ed2ad44704e4c329d88d1a69e2232cb10f
MN-nagy <mahmoudnagimohamed@gmail.com>
2025-12-15 19:34:59 +0200

    day4 b

--- 2025/day4/b.ts
+++ 2025/day4/b.ts
@@ -9,9 +9,11 @@ Goal: get num paper rolls
 const INPUT = "./input.txt";
 const TEST_INPUT = "./testinput.txt";
 
-const content = fs.readFileSync(TEST_INPUT, "utf-8").trim();
+const content = fs.readFileSync(INPUT, "utf-8").trim();
 
-const paperRollGrid = content.split("\n");
+const paperRollGrid: string[][] = content
+  .split("\n")
+  .map((raw) => raw.split(""));
 
 const directions = [
   [-1, -1],
@@ -25,27 +27,38 @@ const directions = [
 ];
 
 let total = 0;
+let removedPaperRolls = 0;
 
-for (let y = 0; y < paperRollGrid.length; y++) {
-  for (let x = 0; x < paperRollGrid[y].length; x++) {
-    if (paperRollGrid[y][x] === "@") {
-      let paperRollCount = 0;
-      for (const direction of directions) {
-        const neighborY = y + direction[0];
-        const neighborX = x + direction[1];
+do {
+  let rollsToRemove: number[][] = [];
+  removedPaperRolls = 0;
+  for (let y = 0; y < paperRollGrid.length; y++) {
+    for (let x = 0; x < paperRollGrid[y].length; x++) {
+      if (paperRollGrid[y][x] === "@") {
+        let paperRollCount = 0;
+        for (const direction of directions) {
+          const neighborY = y + direction[0];
+          const neighborX = x + direction[1];
 
-        if (isValidDirection(neighborY, neighborX, paperRollGrid[y].length)) {
-          if (paperRollGrid[neighborY][neighborX] === "@") {
-            paperRollCount += 1;
+          if (isValidDirection(neighborY, neighborX, paperRollGrid[y].length)) {
+            if (paperRollGrid[neighborY][neighborX] === "@") {
+              paperRollCount += 1;
+            }
           }
         }
-      }
-      if (paperRollCount < 4) {
-        total += 1;
+        if (paperRollCount < 4) {
+          total += 1;
+          rollsToRemove.push([y, x]);
+          removedPaperRolls += 1;
+        }
       }
     }
   }
-}
+
+  for (const toBeRemoved of rollsToRemove) {
+    paperRollGrid[toBeRemoved[0]][toBeRemoved[1]] = ".";
+  }
+} while (removedPaperRolls);
 
 function isValidDirection(
   nY: number,
