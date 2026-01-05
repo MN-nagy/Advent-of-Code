8b01beb26068c05c348f0dd6274cae8640c65a28
MN-nagy <mahmoudnagimohamed@gmail.com>
2026-01-05 14:20:40 +0200

    day13

---
+++ 2024/Cplusplus/day13/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -xe
+
+# clang++ --std=c++20 -Wall -Wextra -Werror -O2 day13a.cpp -o day13a
+clang++ --std=c++20 -Wall -Wextra -Werror -O2 day13b.cpp -o day13b
---
+++ 2024/Cplusplus/day13/day13a.cpp
@@ -0,0 +1,46 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+typedef long long ll;
+
+#define REAL "day13.txt"
+#define TEST "test.txt"
+#define sz(x) ((int)x.size())
+
+void solvep2() {
+  ll ax, ay, bx, by, px, py;
+  ll t_tokens = 0;
+
+  while (scanf("Button A: X+%lld, Y+%lld\n", &ax, &ay) == 2) {
+    scanf("Button B: X+%lld, Y+%lld\n", &bx, &by);
+    scanf("Prize: X=%lld, Y=%lld\n", &px, &py);
+
+    ll det = ax * by - ay * bx;
+    ll na = px * by - py * bx;
+    ll nb = py * ax - px * ay;
+
+    if (det == 0)
+      continue;
+    if (na % det != 0 || nb % det != 0)
+      continue;
+
+    ll a = na / det;
+    ll b = nb / det;
+
+    if (a < 0 || b < 0 || a > 100 || b > 100)
+      continue;
+
+    t_tokens += (a * 3 + b * 1);
+  }
+
+  cout << t_tokens << "\n";
+}
+
+int main() {
+  freopen(REAL, "r", stdin);
+
+  solvep2();
+
+  return 0;
+}
